a1ccbafd5c223852d88408a4af8df55d3ad7f937
ScoBoFoSho <ferrecisco@icloud.com>
2022-06-30 23:26:53 -0400

    First Round/Debugging Needed!

--- Develop/script.js
+++ Develop/script.js
@@ -35,19 +35,15 @@ function generatePassword () {
     }
   }
 // the loop belongs here
-// if ( lowerLetters || uppercaseLetters || numFigures || specialCharacters ); {
-  //if they did then lets get some passwords going
+if ( lowerLetters || uppercaseLetters || numFigures || specialCharacters ); {
+  // if they did then lets get some passwords going
 
 let password = ""
 for (var i = 0; i <= passElements.promptlength; i++) {
   var randomNumber = Math.floor(Math.random() * empytArrary,length);
    password += empytArray[randomNumber]
  }
-;
-// else {
-//   generatePassword ()
-// };
-
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -55,7 +51,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   debugger;
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PasswordGen
-A link to a wep page that generates a random password
+### A link to a wep page that generates a random password
+### This page uses HTML, CSS, and JavaScript to create a password.
+### This current itteration does not work properly, but it's being debugged to create an effective and usable tool
+<break>
+<break>
+Link to the Page: (https://scobofosho.github.io/PasswordGen/)
+Link to the Repo: (https://github.com/ScoBoFoSho/PasswordGen)
+
